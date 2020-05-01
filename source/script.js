6bd243502ebc92ee5ef14c44f78f9955d1863a92
Max S. Haberman <max@surveycto.com>
2020-05-01 16:45:11 -0400

    Initial adding of files

---
+++ source/script.js
@@ -0,0 +1,23 @@
+function keypress (e) {
+  const key = e.key
+  if (key == continueKey) {
+    setAnswer('1')
+    goToNextField()
+  }
+}
+
+function clearAnswer () {
+  setAnswer('')
+}
+
+/* global setAnswer, goToNextField, getPluginParameter, fieldProperties */
+
+var continueKey = getPluginParameter('key')
+var allowBack = getPluginParameter('continue')
+var currentAnswer = fieldProperties.CURRENT_ANSWER
+
+if ((allowBack != 1) && (currentAnswer != null)) { // If not allowed to go back here, and there is already a value, then skips forward
+  goToNextField()
+}
+
+document.addEventListener('keyup', keypress)
